4d0d5850872364ee89b21f0980224b25a3f9cd66
thiago25104708360023-prog <thiago.25104708360023@faeterj-rio.edu.br>
2026-07-06 09:34:42 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -72,7 +72,7 @@ INSERT INTO services (category_id, name, price) VALUES
 (4, 'Nutrição Capilar', 100.00),
 (4, 'Limpeza De Pele', 120.00);
 
---email pra testar
+
 INSERT INTO users (name, email, password) VALUES 
 ('Cliente Teste', 'luminebeautyofc@gmail.com', '$2y$10$7R93Wf7eLwYh0GvT.5pfeOX2Wq7v1O1t67R3M2/oV3pEq3.tD.YWW')
 ON DUPLICATE KEY UPDATE email=email;
